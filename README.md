169f5438d6ba76578f5e801b9957f56288677567
Ben Backus <bbackus@Bens-MacBook-Pro.local>
2015-08-15 10:01:46 -0400

    PyTools-3 add json tools module

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ As of right now, it contains:
 		- writes to csv file date, in/out times, time worked
 		- bug fixes:
 			- fixed bug where 10:01 would look like 10:1 for example 
+    - A json tools module
+        - Formatting function to make json readable to humans
+        - Function that aggregates dictionaries
+        - bug fixes:
+            - Fixed bug where formatting would not indent dictionaries
+            - Changed json tools from script to a module
---
+++ json_tools.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import json
+
+filepath = sys.argv[1]
+file = open(filepath)
+json_info = json.load(file)
+indent = "   "
+
+def pr(thing_to_print, num_indents):
+    s = indent * num_indents
+    return s + thing_to_print
+
+def format(js, indents):
+    formatted = ""
+    formatted += pr("{\n", indents)
+    for item in js:
+        valtype = type(js[item]).__name__
+        if valtype == 'dict':
+            formatted += pr('"'+item+'"'+':\n', indents+1)
+            formatted += format(js[item], indents+1)
+        elif valtype == 'unicode':
+            formatted += pr('"{}": "{}",\n'.format(item, js[item]), indents + 1)
+        else:
+            formatted += pr('"{}": {},\n'.format(item, js[item]), indents + 1)
+    if indents == 0:
+        formatted += pr("}", indents)
+    else:
+        formatted += pr("},\n", indents)
+    return formatted
+
+def combineDicts(dicts):
+    ret = {}
+    for d in dicts:
+        for item in d:
+            valtype = type(d[item]).__name__
+            try:
+                if valtype == 'list':
+                    ret[item].extend(d[item])
+                    ret[item] = list(set(ret[item]))
+                elif valtype == 'dict':
+                    ret[item] = combineDicts([ret[item], d[item]])
+                else:
+                    ret[item].append(d[item])
+            except KeyError:
+                if valtype == 'list' or valtype == 'dict':
+                    ret[item] = d[item]
+                else:
+                    ret[item] = [d[item]]
+    return ret
+
